de7c2b0c779b0e3b2838a1ca301424f962a7ef48
Upendra2003 <upendrakarimi2003@gmail.com>
2023-10-01 22:43:58 +0530

    frontend registration completed

--- backend/Profile/urls.py
+++ backend/Profile/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 from . import views
 from rest_framework_simplejwt.views import (
-    TokenObtainSlidingView,
-    TokenRefreshSlidingView,
+    TokenObtainPairView,
+    TokenRefreshView,
 )
 
 urlpatterns=[
 
-    path('user/token/', TokenObtainSlidingView.as_view(), name='token_obtain'),
-    path('user/token/refresh/', TokenRefreshSlidingView.as_view(), name='token_refresh'),
+    path('user/token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/',views.getUsers,name='get-user'),
     path('user/<str:id>/',views.getUser,name='get-User'),
     path('user/<str:id>/delete/',views.deleteuser,name='get-delete-User'),
+
+    
     path('register/',views.register_userProfile,name='register'),
 ]
