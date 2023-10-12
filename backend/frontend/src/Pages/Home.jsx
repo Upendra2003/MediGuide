e71accebd11b17bd861f0efc9bc90246078b84e3
Royal-Dragon <b221025@iiit-bh.ac.in>
2023-10-12 18:15:09 +0530

    hiw page complete

--- backend/frontend/src/Pages/Home.jsx
+++ backend/frontend/src/Pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Img from '../assets/hero-img1.png'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
@@ -6,17 +8,17 @@ const Home = () => {
     <div className="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
         <div className="grid items-center grid-cols-1 gap-y-6 md:grid-cols-2 md:gap-x-20">
             <div className="">
-                <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">Empowering Your Health Journey with Precision and Care</h2>
-                <p className="mt-4 text-base leading-relaxed text-gray-600">Guiding Your Health Choices with Data-Driven Precision and Compassion</p>
+                <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">“MedSP”- a all new digital way of next level first-aid for everyone</h2>
+                <p className="mt-4 text-base leading-relaxed text-gray-400">Our platform leverages cutting-edge technology to provide you with accurate and timely medical guidance, right from the comfort of your own home</p>
                 <div className='mt-4' >
-                  <button className='rounded-md bg-slate-800 px-7 py-2.5  text-white '>Get Started</button>
-                  <button className='ml-4 font-medium'> How it Work</button>
+                  <Link to={"/predict"} className='rounded-md bg-slate-800 px-7 py-2.5  text-white '>Predictor</Link>
+                  <Link to={'/Hiw'} className='ml-4 font-medium'> How it Work</Link>
                 </div>
             </div>
 
             <div className="relative pl-20 pr-6 sm:pl-6 md:px-0">
                 <div className="relative w-full max-w-xs mt-4 mb-10 ml-auto">
-                    <img className="ml-auto scale-150 " src='https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQQU9IewO12C_F3rj5gjbA97OswkaokgaOJDHhiw1Pad1s7y3Il' />
+                    <img className="ml-auto scale-150 " src={Img} />
 
                     <img className="absolute -top-4 -left-12" src="" alt="" />
                 </div>
--- backend/frontend/src/components/Footer.jsx
+++ backend/frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
 return (
-    <section class="py-6 bg-slate-800  px-52">
+    <section class="py-6 bg-neutral-800  px-52">
     <div class="">
         <div class="flex flex-col ">
             <div class="flex justify-between">
