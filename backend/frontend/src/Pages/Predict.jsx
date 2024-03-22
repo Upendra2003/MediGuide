735dc7bb09da56a16ea83801a18d23b80658cc3f
Upendra2003 <b121023@iiit-bh.ac.in>
2024-03-22 21:17:30 +0530

    updated maps

--- backend/frontend/src/Pages/Predict.jsx
+++ backend/frontend/src/Pages/Predict.jsx
@@ -4,6 +4,7 @@ import symptomsData from './symptoms';
 import '../components/modal.css';
 import bg from '../assets/Background.png'
 import bg2 from '../assets/Pattern.png'
+import { Link } from 'react-router-dom'
 
 export default function Predict() {
     const [symptomsList, setSymptomsList] = useState([]);
@@ -215,6 +216,14 @@ export default function Predict() {
                         <button class="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none " onClick={handleCardClick}>
                         Go back
                         <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+                        </button>
+                        <button type='submit' onClick={togglemodal} class="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group">
+                            <span class="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
+                            <span class="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
+                            <span class="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+                            <span class="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+                            <span class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
+                            <span class="relative transition-colors duration-300 delay-200 group-hover:text-white ease"><Link to={'/hospitals'}>Nearby Hospitals</Link></span>
                         </button>
                     </div>
                 ) : (
